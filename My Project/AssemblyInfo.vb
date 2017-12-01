ed7f098327ce66ca55eeb3c9b2130b8d4a39a911
Jacek Dziwoki <ide@konto.pl>
2017-12-01 22:55:18 +0100

    Uruchomienie Git

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Beret")> 
+<Assembly: AssemblyDescription("Nakładka na system RaksSQL")> 
+<Assembly: AssemblyCompany("www.INFIDO.pl")> 
+<Assembly: AssemblyProduct("Beret")> 
+<Assembly: AssemblyCopyright("Copyright ©  2010-2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("40fb53ef-175c-4882-b841-cc1660fa8524")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2.1.2.23")> 
+<Assembly: AssemblyFileVersion("2.1.2.23")> 
